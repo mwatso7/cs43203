da090c295825dc589d54ab7e21fb50f0dabc4d07
mwatso7 <mwatso7@kent.edu>
2014-04-02 20:18:20 -0400

    Update fork.c
    changes to make the program a basic daemon

--- examples/fork.c
+++ examples/fork.c
@@ -7,25 +7,38 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
-	pid_t p;
-	int i;
+	pid_t p, sid;
+	int log_fd;
 
 	p = fork();	
-
-	printf("Hello\n");
-
-	if( p != 0 ){
-		printf("We\'re waiting...\n");
-		wait(NULL);
-	}else{
-		for( i = 0; i < 5; i++ ){
-			printf("I\'m the child still running\n");
-			sleep(5);
-		}
+	
+	if( p < 0 ){
+		exit(EXIT_FAILURE);
 	}
-
+	if( p < 0 ){
+		exit(EXIT_SUCCESS);
+	}
+	
+	unmask(022);
+	log_fd = open("/dev/null", O_WRONLY);
+	close(STDERR_FILENO);
+	dup(log_fd);
+	close(STDOUT_FILENO);
+	dup(log_fd);
+	close(STDIN_FILENO);
+	
+	sid = setsid()
+	if( sid < 0){
+		exit(EXIT_FAILURE);		
+	}
+	
+	while(1){
+		sleep(1);
+	}
 	printf("Done %d\n",p);	
 
 	return 0;
